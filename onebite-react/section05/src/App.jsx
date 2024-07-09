d5a6dd0bc73bc57629eff2dfb2a6c10016422ed2
yunju <yunj333@gmail.com>
2024-07-09 17:49:09 +0900

    한입리액트section05

---
+++ onebite-react/section05/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Main from "./components/Main";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+      <h1>안녕 리액트!</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ onebite-react/section05/src/components/Button.jsx
@@ -0,0 +1,10 @@
+const Button = ({ text, color = "black", children }) => {
+  return (
+    <button style={{ color: color }}>
+      {text} - {color.toUpperCase()}
+      {children}
+    </button>
+  );
+};
+
+export default Button;
