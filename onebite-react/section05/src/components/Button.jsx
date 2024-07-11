c0617dfc7f6e35f31a1159aa4156d317959c5414
yunj33@naver.com <114348883+yujachaa@users.noreply.github.com>
2024-07-12 00:55:58 +0900

    2024.07.12 학습완료

--- onebite-react/section05/src/components/Button.jsx
+++ onebite-react/section05/src/components/Button.jsx
@@ -1,6 +1,15 @@
 const Button = ({ text, color, children }) => {
+  //이벤트 객체
+  const onClickButton = (e) => {
+    console.log(e);
+    console.log(text);
+  };
   return (
-    <button style={{ color: color }}>
+    <button
+      onClick={onClickButton}
+      // onMouseEnter={onClickButton}
+      style={{ color: color }}
+    >
       {text} - {color.toUpperCase()}
       {children}
     </button>
